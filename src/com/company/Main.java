bae15f2b8fa556a801e9ee43c5a65c02ea67b436
n2d <nikitos75091@gmail.com>
2022-03-20 17:29:24 +0900

    Сделал рабочую версию калькулятора

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import com.company.Exceptions.IncorrectInputExceptions;
+import com.company.Utils.CalculatorUtils;
+import com.company.Utils.CalculatorUtilsImpl;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CalculatorUtils calculatorUtils = new CalculatorUtilsImpl();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите уравнение :");
+        String input = scanner.nextLine();
+        String[] inputArr = input.split(" ");
+
+        if (inputArr.length != 3) {
+            throw new IncorrectInputExceptions("Выввели неверное выражение, ваше выражние : " + Arrays.toString(inputArr));
+        }
+        calculatorUtils.startCalculate(inputArr[0], inputArr[1], inputArr[2]);
+    }
+}
---
+++ src/com/company/Utils/CalculatorUtils.java
@@ -0,0 +1,5 @@
+package com.company.Utils;
+
+public interface CalculatorUtils {
+    public void startCalculate(String number_1, String operator, String number_2);
+}
